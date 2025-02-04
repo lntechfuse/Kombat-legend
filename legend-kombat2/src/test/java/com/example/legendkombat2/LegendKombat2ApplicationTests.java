76659bef281c660b4cda5f682843b0e645944ff8
Harukaah <sirapobymw@gmail.com>
2025-02-04 14:14:02 +0700

    YEAAAH

---
+++ legend-kombat2/src/test/java/com/example/legendkombat2/LegendKombat2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.legendkombat2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendKombat2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
